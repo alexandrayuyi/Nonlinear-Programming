0333242350b2f3398ff7172feb4da6c300d424d9
Astrou Lui <luisdaniel16an@gmail.com>
2024-11-28 20:59:08 -0400

    fix: changes the text in print

--- ProblemasMasGrandes.py
+++ ProblemasMasGrandes.py
@@ -21,6 +21,6 @@ if __name__ == "__main__":
     problem = ProblemaMasGrande(f_value=x**2 + 4*x + 5, restriccion=[x >= 2, x <= 5])
 
     # Resultados
-    print("Minimo__", problem.getSolver())
-    print("Minimo__",x.value)
+    print("Problema", problem.getSolver())
+    print("Valor optimo de x",x.value)
 
